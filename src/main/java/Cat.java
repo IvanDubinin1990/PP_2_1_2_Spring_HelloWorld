0eef872aab7a55719b7326acffd29b8c95b38b18
Кристина <dubininivan1990@gmail.com>
2024-03-12 10:37:08 +0400

    Выполнено задание ПП 2.1.2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,57 @@
+import java.util.Objects;
+
+public class Cat {
+
+    private String name;
+    private String color;
+    private int age;
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return age == cat.age && Objects.equals(name, cat.name) && Objects.equals(color, cat.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color, age);
+    }
+}
